1ce1901093b1686c7194c398b7f09e258d8e38eb
Stefan Jucker <stefan.jucker@gmail.com>
2025-10-19 16:14:53 +0200

    Initialize Nuxt project with basic configuration and dependencies

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <UApp>
+    <UHeader/>
+
+    <UMain>
+      <NuxtLayout>
+        <h1>Das nächste Schlierefäscht findet vom 3. bis 12. September 2027 statt</h1>
+        <UIcon name="i-lucide-lightbulb" class="size-5"/>
+        <NuxtImg src="/front.jpg" densities="x1 x2" class="rounded-sm"/>
+      </NuxtLayout>
+    </UMain>
+
+    <UFooter/>
+  </UApp>
+</template>
+<script setup lang="ts">
+</script>
