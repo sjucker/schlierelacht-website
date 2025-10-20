1f02592b677b9792ad4ad87e4f8db0d3323bcef7
Stefan Jucker <stefan.jucker@gmail.com>
2025-10-20 11:31:44 +0200

    Enhance project with typography plugin

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,21 @@
 <template>
   <UApp>
-    <UHeader/>
 
-    <UMain>
-      <NuxtLayout>
-        <h1>Das nächste Schlierefäscht findet vom 3. bis 12. September 2027 statt</h1>
-        <UIcon name="i-lucide-lightbulb" class="size-5"/>
-        <NuxtImg src="/front.jpg" densities="x1 x2" class="rounded-sm"/>
-      </NuxtLayout>
-    </UMain>
+    <div class="container flex flex-col items-center md:flex-row md:items-stretch md:gap-12 m-auto prose md:prose-2xl">
+      <div class="flex-1">
+        <NuxtImg src="/logo.svg" densities="x1 x2"/>
+      </div>
+      <div class="flex-3">
+        <h3>Herzlich willkommen am Schlierefäscht vom 3. bis 12. September 2027</h3>
+        <NuxtImg src="/front.jpg" densities="x1 x2" class="w-full rounded-md"/>
 
-    <UFooter/>
+        <h4>Kontakt</h4>
+        <h4>Downloads</h4>
+        <h5>Logos</h5>
+        <h5>Sponsoring</h5>
+        <h5>Gastro</h5>
+      </div>
+    </div>
   </UApp>
 </template>
 <script setup lang="ts">
