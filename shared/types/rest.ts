88b5a95885545d5788bbd6ee9694dacd174af6b6
Stefan Jucker <stefan.jucker@gmail.com>
2026-02-23 10:21:26 +0100

    Add generated typescript types

---
+++ shared/types/rest.ts
@@ -0,0 +1,53 @@
+/* eslint-disable */
+
+export interface ArtistDTO {
+    externalId: string;
+    name: string;
+    description: string;
+    website?: string;
+    instagram?: string;
+    facebook?: string;
+    youtube?: string;
+    images: ImageDTO[];
+    programm: ProgrammEntryDTO[];
+}
+
+export interface ImageDTO {
+    cloudflareId: string;
+    description: string;
+    type: ImageType;
+}
+
+export interface LocationDTO {
+    externalId: string;
+    type: LocationType;
+    name: string;
+    latitude: number;
+    longitude: number;
+    googleMapsUrl: string;
+    cloudflareId?: string;
+    mapId?: string;
+}
+
+export interface ProgrammEntryDTO {
+    location: LocationDTO;
+    fromDate: DateAsString;
+    fromTime?: DateAsString;
+    toDate?: DateAsString;
+    toTime?: DateAsString;
+}
+
+export type DateAsString = string;
+
+export enum ImageType {
+    MAIN = "MAIN",
+    ADDITIONAL = "ADDITIONAL",
+}
+
+export enum LocationType {
+    STAGE = "STAGE",
+    FOOD_STAND = "FOOD_STAND",
+    BAR = "BAR",
+    TENT = "TENT",
+    ATTRACTION = "ATTRACTION",
+}
