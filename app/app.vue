abda8a24e17d83ea894f9c52be62d3ec385d4f30
Stefan Jucker <jucker@mptechnology.ch>
2026-03-04 16:00:17 +0100

    Embed Facebook video player on the Schlierefäscht landing page.

--- app/app.vue
+++ app/app.vue
@@ -90,6 +90,13 @@
           </div>
         </div>
 
+        <div class="flex flex-row justify-center pt-4">
+          <iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fschlierelacht%2Fvideos%2F3538727499751138%2F&show_text=false&width=267&t=0"
+                  width="347"
+                  height="619"
+                  style="border:none;overflow:hidden;max-width: 100%;" scrolling="no" frameborder="0" allowfullscreen="true"
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </div>
       </div>
     </div>
 
