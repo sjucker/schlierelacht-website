db47f346fecde5daf964bba85061a16efe2e823d
Stefan Jucker <stefan.jucker@gmail.com>
2026-07-06 16:59:48 +0200

    Adjust Impressum

--- app/pages/impressum.vue
+++ app/pages/impressum.vue
@@ -6,25 +6,30 @@
     <div class="prose max-w-none">
       <h3>Inhaltliche Verantwortung</h3>
       <p>
-        OK Schlierefäscht 2027 „Schliere lacht"<br>
-        Geschäftsstelle, Charly Mettier, 079 456 79 69<br>
+        eventSCHLIEREN, Organisation Schlierefäscht 2027<br>
+        Freiestrasse 6, Stadthaus, 8952 Schlieren
+      </p>
+      <p>
+        <strong>Geschäftsstelle Schlierefäscht 2027</strong><br>
+        Charly Mettier<br>
         Kampstrasse 6, 8952 Schlieren<br>
-        E-Mail: <a href="mailto:info@schlierelacht.ch">info@schlierelacht.ch</a>
+        <a href="tel:+41794567969">079 456 79 69</a><br>
+        <a href="mailto:info@schlierelacht.ch">info@schlierelacht.ch</a>
       </p>
 
-      <h4>Haftungsausschluss</h4>
+      <h3>Haftungsausschluss</h3>
       <p>Der Autor übernimmt keinerlei Gewähr hinsichtlich der inhaltlichen Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen. Haftungsansprüche gegen den
         Autor wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch
         technische Störungen entstanden sind, werden ausgeschlossen. Alle Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne
         gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
 
-      <h4>Haftung für Links</h4>
+      <h3>Haftung für Links</h3>
       <p>Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher
         Webseiten erfolgen auf eigene Gefahr des Nutzers oder der Nutzerin.</p>
 
-      <h4>Urheberrechte</h4>
-      <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören ausschliesslich der Firma Mettier Werbung+Kommunikation oder den speziell
-        genannten Rechtsinhabern. Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.</p>
+      <h3>Urheberrechte</h3>
+      <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören ausschliesslich dem Verein eventSCHLIEREN/Fototeams Schlierefäscht oder den
+        speziell genannten Rechtsinhabern. Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.</p>
     </div>
   </div>
 </template>
